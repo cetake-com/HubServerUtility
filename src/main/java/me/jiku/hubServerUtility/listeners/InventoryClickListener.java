d81d6e7a273fd87645390cb72908cdd7af2ab806
Jiku0123 <jiku0123.sha456@gmail.com>
2026-03-18 01:02:11 +0900

    Develop (#7)
    * third commit
    
    * edit click handler
    
    * edit gitignore
    
    * no done
    
    * no done
    
    * done
    
    * Delete build directory

--- src/main/java/me/jiku/hubServerUtility/listeners/InventoryClickListener.java
+++ src/main/java/me/jiku/hubServerUtility/listeners/InventoryClickListener.java
@@ -2,11 +2,13 @@ package me.jiku.hubServerUtility.listeners;
 
 import me.jiku.hubServerUtility.HubServerUtility;
 import me.jiku.hubServerUtility.utlis.GUIUtils;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+
 
 public class InventoryClickListener implements Listener{
 
@@ -21,28 +23,44 @@ public class InventoryClickListener implements Listener{
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent e){
-        Player player = (Player) e.getWhoClicked();
 
-        e.setCancelled(true);
+        Player player = (Player) e.getWhoClicked();
+        var config = plugin.getConfig();
+
 
         e.getCurrentItem();
 
-        if(e.getCurrentItem() != null) return;
+        if(e.getCurrentItem() == null) return;
 
         if(e.getView().getTitle().equalsIgnoreCase(MAINMENU)){
 
+            e.setCancelled(true);
+
+            //if you want to add setting, you add option item here
             switch(e.getCurrentItem().getType()){
                 case ENDER_EYE:
-                    GUIUtils.openOptionMenu(player);
-                    if(e.getCurrentItem().getType() == Material.GREEN_CONCRETE){
-                        GUIUtils.openMainMenu(player);
-                        player.sendMessage("Enable");
-                    }else{
-                        GUIUtils.openMainMenu(player);
-                        player.sendMessage("Disable");
-                    }
-
+                    GUIUtils.openOptionMenu(player, Material.ENDER_EYE);
                     break;
+            }
+
+        }else if(e.getView().getTitle().equalsIgnoreCase(OPTIONMENU)){
+
+            e.setCancelled(true);
+
+            //if you added option item set behavior
+            if(e.getClickedInventory().contains(Material.ENDER_EYE)){
+                switch(e.getCurrentItem().getType()){
+                    case GREEN_CONCRETE:
+                        player.sendMessage(ChatColor.GREEN + "Option was Enabled!");
+                        config.set("init-spawn-setting", true);
+                        plugin.saveConfig();
+                        break;
+                    case RED_CONCRETE:
+                        player.sendMessage(ChatColor.RED + "Option was Disabled!");
+                        config.set("init-spawn-setting", false);
+                        plugin.saveConfig();
+                        break;
+                }
             }
         }
     }
